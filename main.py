0f6595acfc5dd8c6a056fe73d0f21ee40701aaea
liyang <liyang@liyangdeMacBook-Pro.local>
2025-04-01 11:17:36 +0800

    忽略机器人自己发的消息

--- main.py
+++ main.py
@@ -60,6 +60,8 @@ class MyPlugin(Star):
     async def on_all_message(self, event: AstrMessageEvent):
         if event.get_platform_name() != "gewechat":
             return
+        if event.get_self_id() == event.get_sender_id():
+            return
 
         sender = event.get_sender_name() if event.get_group_id() != "" else event.get_sender_id()
         target = event.get_group_id() if event.get_group_id() != "" else event.get_sender_id()
